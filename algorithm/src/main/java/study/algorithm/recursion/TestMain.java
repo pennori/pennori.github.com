0647d17ad190dcf92f130e341fbb0f0a22b95412
sonhoseong <pennori@nate.com>
2014-08-06 18:58:48 +0900

    .

--- algorithm/src/main/java/study/algorithm/recursion/TestMain.java
+++ algorithm/src/main/java/study/algorithm/recursion/TestMain.java
@@ -11,10 +11,28 @@ public class TestMain {
 		testMain.print(arr);
 	}
 
-	/**
-	 * @param arr
-	 */
 	private void print(Object[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			Object outer = arr[i];
+
+			if (outer instanceof Integer) {
+				System.out.print(outer);
+				if (i < arr.length - 1) {
+					System.out.print(", ");
+				}
+				continue;
+			}
+
+			Object[] inner = (Object[]) outer;
+
+			for (int j = 0; j < inner.length; j++) {
+				System.out.print(inner[j] + ", ");
+			}
+
+		}
+	}
+	
+	private void executetRecursionFunction(Object[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			Object outer = arr[i];
 
@@ -36,8 +54,7 @@ public class TestMain {
 	}
 
 	Object[] getObjectArray() {
-		Object[] arr = { 1, 2, new Object[] { 1, 3, 5 }, 45, 7, 8,
-				new Object[] { 9, 10, 13 }, 5, 2 };
+		Object[] arr = { 1, 2, new Object[] { 1, 3, 5 }, 45, 7, 8, new Object[] { 9, 10, 13 }, 5, 2 };
 		return arr;
 	}
 
