73dc8410c69c8049b882ec709938c944f2d1aa82
sonhoseong <pennori@nate.com>
2014-08-08 17:24:12 +0900

    .

--- algorithm/src/main/java/study/algorithm/TenStairs/TestMain.java
+++ algorithm/src/main/java/study/algorithm/TenStairs/TestMain.java
@@ -9,15 +9,24 @@ public class TestMain {
 	}
 
 	public void execute() {
-		System.out.println(ways(10));
+		System.out.println("recursion : " + recursionWays(10));
+		System.out.println("another : " + anotherWays(10));
 	}
 
-	public int ways(int n) {
+	public int recursionWays(int n) {
 		if (1 == n)	return 1;
 		if (2 == n)	return 2;
 		if (3 == n)	return 4;
 
-		return ways(n - 1) + ways(n - 2) + ways(n - 3);
+		return recursionWays(n - 1) + recursionWays(n - 2) + recursionWays(n - 3);
+	}
+	
+	public int anotherWays(int n){
+		if (1 == n)	return 1;
+		if (2 == n)	return 2;
+		if (3 == n)	return 4;
+		
+		return 0;
 	}
 
 }
