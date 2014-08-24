e424d56b07edf01dbf0b4f3e047a3d03f9b032f4
sonhoseong <pennori@nate.com>
2014-08-24 15:03:05 +0900

    .

--- algorithm/src/main/java/study/algorithm/sort/insertSort/TestMain.java
+++ algorithm/src/main/java/study/algorithm/sort/insertSort/TestMain.java
@@ -7,9 +7,47 @@ public class TestMain {
 		TestMain testMain = new TestMain();
 		testMain.execute();
 	}
-	
-	public void execute(){
-		System.out.println("insert sort");
+
+	public void execute() {
+		int[] unsort = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		int temp = 0;
+		int round = 0;
+
+		for (int i = 0; i < unsort.length; i++) {
+
+			for (int j = i + 1; j < unsort.length; j++) {
+
+				for (int k = i; k >= 0; k--) {
+					
+					System.out.println(unsort[k] + " VS " + unsort[j]);
+
+					if (unsort[k] > unsort[j]) {
+						
+						System.out.println(unsort[k] + " <==> " + unsort[j]);
+						temp = unsort[k];
+						unsort[k] = unsort[j];
+						unsort[j] = temp;
+
+					} else {
+						
+						break;
+					
+					}
+
+					round++;
+
+				}
+
+			}
+
+		}
+
+		System.out.println(round + "회 비교");
+
+		for (int i = 0; i < unsort.length; i++) {
+			System.out.print(unsort[i] + " ");
+		}
+
 	}
 
 }
