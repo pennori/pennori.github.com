d5cfb2371ce6a7420823d3df0dd066db72e23b3d
pennori <pennori@nate.com>
2015-02-08 16:50:00 +0900

    String hadoop?

--- algorithm/src/main/java/study/algorithm/sortStringPerLine/TestMain.java
+++ algorithm/src/main/java/study/algorithm/sortStringPerLine/TestMain.java
@@ -1,5 +1,8 @@
 package study.algorithm.sortStringPerLine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -9,7 +12,22 @@ public class TestMain {
 	}
 
 	public void execute() {
-		System.out.println(2^10);
+		String words = "내가 그린 기린 그림은 잘 그린 그림이고 니가 그린 기린 그림은 못 그린 기린 그림이다";
+
+		String[] wordArray = words.split("\\s");
+
+		System.out.println(wordArray.length);
+		Map<String, Integer> map = new HashMap<String, Integer>();
+
+		for (String word : wordArray) {
+
+			if (map.containsKey(word)) {
+				map.put(word, map.get(word) + 1);
+			} else {
+				map.put(word, 1);
+			}
+
+		}
 	}
 
 }
