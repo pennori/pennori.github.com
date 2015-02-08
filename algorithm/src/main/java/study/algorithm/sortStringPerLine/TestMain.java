1b45727abe80d1ee9afe058f2a79c250dbbbf28d
sonhoseong <pennori@nate.com>
2015-02-09 02:26:42 +0900

    String hadoop?

--- algorithm/src/main/java/study/algorithm/sortStringPerLine/TestMain.java
+++ algorithm/src/main/java/study/algorithm/sortStringPerLine/TestMain.java
@@ -1,7 +1,9 @@
 package study.algorithm.sortStringPerLine;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestMain {
 
@@ -14,18 +16,27 @@ public class TestMain {
 	public void execute() {
 		String words = "내가 그린 기린 그림은 잘 그린 그림이고 니가 그린 기린 그림은 못 그린 기린 그림이다";
 
-		String[] wordArray = words.split("\\s");
+		String[] array = words.replaceAll("\\s", "").split("");
+		int length = array.length;
 
-		System.out.println(wordArray.length);
-		Map<String, Integer> map = new HashMap<String, Integer>();
+		Set<String> set = new TreeSet<String>();
 
-		for (String word : wordArray) {
+		for (int i = 0; i < length; i++) {
 
-			if (map.containsKey(word)) {
-				map.put(word, map.get(word) + 1);
-			} else {
-				map.put(word, 1);
+			int count = 0;
+
+			if (array[i].equals("") || !set.add(array[i])) {
+				continue;
 			}
+
+			Pattern p = Pattern.compile(array[i]);
+			Matcher m = p.matcher(words);
+
+			while (m.find()) {
+				count++;
+			}
+
+			System.out.println(array[i] + " : " + count);
 
 		}
 	}
