016ca345294f9582a63e4d4bfb0e59910999769d
sonhoseong <pennori@nate.com>
2015-02-13 19:15:16 +0900

    tripleIntCase

---
+++ algorithm/src/main/java/study/algorithm/tripleIntCase/OldTestMain.java
@@ -0,0 +1,142 @@
+package study.algorithm.tripleIntCase;
+
+
+public class OldTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OldTestMain testMain = new OldTestMain();
+
+		long start = System.currentTimeMillis();
+		System.out.println("AA + BC = 100 인 경우의 수 찾기");
+		testMain.practice01();
+		System.out.println("소요 (ms) : " + (System.currentTimeMillis() - start));
+
+		System.out.println();
+
+		start = System.currentTimeMillis();
+		System.out.println("XYZ + XY + 6PP 인 경우의 수 찾기");
+		testMain.practice02();
+		System.out.println("소요 (ms) : " + (System.currentTimeMillis() - start));
+
+		System.out.println();
+
+	}
+
+	private void practice02() {
+		int count = 0;
+		int loop = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+			if (i == 0) {
+				continue;
+			}
+
+			for (int j = 0; j < 10; j++) {
+
+				if (i == j) {
+					continue;
+				}
+
+				loop++;
+
+				for (int k = 0; k < 10; k++) {
+
+					if (k == i || k == j) {
+						continue;
+					}
+
+					String x = String.valueOf(i);
+					String y = String.valueOf(j);
+					String z = String.valueOf(k);
+
+					int xyz = Integer.parseInt(x + y + z);
+					int xy = Integer.parseInt(x + y);
+
+					String result = String.valueOf(xyz + xy);
+
+					if (3 != result.length()) {
+						continue;
+					}
+
+					if (0 != result.indexOf("6")) {
+						continue;
+					}
+
+					if (!result.substring(1, 2).equals(result.substring(2))) {
+						continue;
+					}
+					
+					if (result.substring(2).equals(x)
+							|| result.substring(2).equals(y)
+							|| result.substring(2).equals(z)) {
+						continue;
+					}
+
+					System.out.println(xyz + " + " + xy + " = " + result);
+					count++;
+
+				}
+
+			}
+
+		}
+
+		System.out.println("경우의 수 : " + count);
+		System.out.println("수행횟수 : " + loop);
+
+	}
+
+	private void practice01() {
+		int count = 0;
+		int loop = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+			if (i == 0) {
+				continue;
+			}
+
+			for (int j = 0; j < 10; j++) {
+
+				if (j == 0) {
+					continue;
+				}
+
+				for (int k = 0; k < 10; k++) {
+
+					if (i == j || i == k) {
+						continue;
+					}
+
+					loop++;
+
+					String p = String.valueOf(i);
+					String q = String.valueOf(j);
+					String r = String.valueOf(k);
+
+					int pp = Integer.parseInt(p + p);
+					int qr = Integer.parseInt(q + r);
+
+					String result = String.valueOf(pp + qr);
+
+					if (!"100".equals(result)) {
+						continue;
+					}
+
+					System.out.println(pp + " + " + qr + " = " + result);
+
+					count++;
+
+				}
+
+			}
+
+		}
+
+		System.out.println("경우의 수 : " + count);
+		System.out.println("수행횟수 : " + loop);
+	}
+
+}
--- algorithm/src/main/java/study/algorithm/tripleIntCase/TestMain.java
+++ algorithm/src/main/java/study/algorithm/tripleIntCase/TestMain.java
@@ -10,20 +10,6 @@ public class TestMain {
 		TestMain testMain = new TestMain();
 
 		long start = System.currentTimeMillis();
-		System.out.println("AA + BC = 100 인 경우의 수 찾기");
-		testMain.practice01();
-		System.out.println("소요 (ms) : " + (System.currentTimeMillis() - start));
-
-		System.out.println();
-
-		start = System.currentTimeMillis();
-		System.out.println("XYZ + XY + 6PP 인 경우의 수 찾기");
-		testMain.practice02();
-		System.out.println("소요 (ms) : " + (System.currentTimeMillis() - start));
-
-		System.out.println();
-
-		start = System.currentTimeMillis();
 		System.out.println("식 형태로 입력 받아서 경우의 수 찾기");
 		testMain.execute("XYZ + XY = 6PP");
 		System.out.println("소요 (ms) : " + (System.currentTimeMillis() - start));
@@ -64,6 +50,19 @@ public class TestMain {
 
 				int value = (int) (Math.random() * 10);
 
+				while (true) {
+
+					boolean isFirstDigit = i == 0 || i == in.indexOf("+") + 1;
+
+					if (isFirstDigit && 0 == value) {
+						System.out.println(value);
+						value = (int) (Math.random() * 10);
+					} else {
+						break;
+					}
+
+				}
+
 				if (-1 != ruleMap.get(c).indexOf("_")) {
 
 					String[] rule = ruleMap.get(c).split("_");
@@ -100,126 +99,11 @@ public class TestMain {
 
 		String left = out.substring(0, out.indexOf("+"));
 		String right = out.substring(out.indexOf("+") + 1, out.indexOf("="));
-		String equal = out.substring(out.indexOf("=") + 1);
+		String misc = out.substring(out.indexOf("=") + 1);
 
-		System.out.println(left + " + " + right + " = " + equal);
+		int intResult = Integer.parseInt(left) + Integer.parseInt(right);
+		System.out.println(left + " + " + right + " = " + intResult);
 
-	}
-
-	private void practice02() {
-		int count = 0;
-		int loop = 0;
-
-		for (int i = 0; i < 10; i++) {
-
-			if (i == 0) {
-				continue;
-			}
-
-			for (int j = 0; j < 10; j++) {
-
-				if (i == j) {
-					continue;
-				}
-
-				loop++;
-
-				for (int k = 0; k < 10; k++) {
-
-					if (k == i || k == j) {
-						continue;
-					}
-
-					String x = String.valueOf(i);
-					String y = String.valueOf(j);
-					String z = String.valueOf(k);
-
-					int xyz = Integer.parseInt(x + y + z);
-					int xy = Integer.parseInt(x + y);
-
-					String result = String.valueOf(xyz + xy);
-
-					if (3 != result.length()) {
-						continue;
-					}
-
-					if (0 != result.indexOf("6")) {
-						continue;
-					}
-
-					if (!result.substring(1, 2).equals(result.substring(2))) {
-						continue;
-					}
-					
-					if (result.substring(2).equals(x)
-							|| result.substring(2).equals(y)
-							|| result.substring(2).equals(z)) {
-						continue;
-					}
-
-					System.out.println(xyz + " + " + xy + " = " + result);
-					count++;
-
-				}
-
-			}
-
-		}
-
-		System.out.println("경우의 수 : " + count);
-		System.out.println("수행횟수 : " + loop);
-
-	}
-
-	private void practice01() {
-		int count = 0;
-		int loop = 0;
-
-		for (int i = 0; i < 10; i++) {
-
-			if (i == 0) {
-				continue;
-			}
-
-			for (int j = 0; j < 10; j++) {
-
-				if (j == 0) {
-					continue;
-				}
-
-				for (int k = 0; k < 10; k++) {
-
-					if (i == j || i == k) {
-						continue;
-					}
-
-					loop++;
-
-					String p = String.valueOf(i);
-					String q = String.valueOf(j);
-					String r = String.valueOf(k);
-
-					int pp = Integer.parseInt(p + p);
-					int qr = Integer.parseInt(q + r);
-
-					String result = String.valueOf(pp + qr);
-
-					if (!"100".equals(result)) {
-						continue;
-					}
-
-					System.out.println(pp + " + " + qr + " = " + result);
-
-					count++;
-
-				}
-
-			}
-
-		}
-
-		System.out.println("경우의 수 : " + count);
-		System.out.println("수행횟수 : " + loop);
 	}
 
 }
