0bab9ce94b5e721e9f6131b7a4256f4e03ef86ef
sonhoseong <pennori@nate.com>
2015-02-16 01:54:38 +0900

    tripleIntCase

--- algorithm/src/main/java/study/algorithm/tripleIntCase/ThirdTestMain.java
+++ algorithm/src/main/java/study/algorithm/tripleIntCase/ThirdTestMain.java
@@ -1,13 +1,21 @@
 package study.algorithm.tripleIntCase;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ThirdTestMain {
 
 	public String p = "XYZ";
 	public String q = "XY";
 	public String r = "6PP";
+	List<Character> strList;
+	Map<Character, Integer> strMap;
+
+	public Set<String> resultSet = new TreeSet<String>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,19 +24,76 @@ public class ThirdTestMain {
 	}
 
 	public void execute() {
-		System.out.println(isStr(p));
-		calc(p, q, r);
+		String pqr = p + q + r;
+		strList = getUniqueStrList(pqr);
+		System.out.println(strList);
+
+		strMap = getUniqueStrMap(pqr);
+		System.out.println(strMap.entrySet());
+
+		calculate(p, q, r);
 	}
 
-	private boolean isStr(String str) {
-		Pattern pattern = Pattern.compile("^[A-Za-z]+");
-		Matcher m = pattern.matcher(str);
-		return m.find();
+	private void calculate(String p, String q, String r) {
+
+
+		for (int i = 0; i < strList.size(); i++) {
+
+			for (int j = 0; j < 10; j++) {
+
+				p = p.replaceAll(String.valueOf(strList.get(i)),
+						String.valueOf(i));
+				q = q.replaceAll(String.valueOf(strList.get(i)),
+						String.valueOf(i));
+				r = r.replaceAll(String.valueOf(strList.get(i)),
+						String.valueOf(i));
+
+			}
+
+		}
+
+		calculate(p, q, r);
 	}
 
-	private void calc(String p, String q, String r) {
-		// TODO Auto-generated method stub
-		return;
+	private Map<Character, Integer> getUniqueStrMap(String input) {
+		Map<Character, Integer> strMap = new HashMap<Character, Integer>();
+		for (int i = 0; i < input.length(); i++) {
+
+			char key = input.charAt(i);
+
+			if (key > 47 && key < 58) {
+				continue;
+			}
+			
+			if (!strMap.containsKey(key)) {
+				strMap.put(key, -1);
+			}
+
+		}
+		return strMap;
+	}
+
+	private List<Character> getUniqueStrList(String input) {
+		List<Character> strList = new ArrayList<Character>();
+		for (int i = 0; i < input.length(); i++) {
+
+			char key = input.charAt(i);
+
+			if (key > 47 && key < 58) {
+				continue;
+			}
+
+			if (!strList.contains(key)) {
+				strList.add(key);
+			}
+
+		}
+		return strList;
+	}
+
+	private boolean isFirstDigit(String p, String q, String r, Character key) {
+		return 0 == p.indexOf(key) || 0 == q.indexOf(key)
+				|| 0 == r.indexOf(key);
 	}
 
 }
