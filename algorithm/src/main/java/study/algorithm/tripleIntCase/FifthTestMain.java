cef9415325ed58f9320e50467fa8514d220c9c9f
sonhoseong <pennori@nate.com>
2015-02-16 23:07:15 +0900

    tripleIntCase

---
+++ algorithm/src/main/java/study/algorithm/tripleIntCase/FifthTestMain.java
@@ -0,0 +1,20 @@
+package study.algorithm.tripleIntCase;
+
+public class FifthTestMain {
+
+	public String strP = "XYZ";
+	public String strQ = "XY";
+	public String strR = "6PP";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FifthTestMain main = new FifthTestMain();
+		main.execute();
+	}
+
+	private void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
