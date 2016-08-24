eb00c3eecc7b460e42e3712e21a30020fc728f27
pennori <pennori@nate.com>
2016-08-24 16:38:16 +0900

    word reverse with stack

--- algorithm/src/test/java/study/algorithm/practice/WordReverseTest.java
+++ algorithm/src/test/java/study/algorithm/practice/WordReverseTest.java
@@ -1,27 +1,49 @@
 package study.algorithm.practice;
 
-import static org.junit.Assert.*;
-
 import java.util.Stack;
 
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class WordReverseTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Solution actual = new Solution();
+		Assert.assertEquals("implemented yet Not", actual.solution("Not yet implemented"));
 	}
-	
+
 	class Solution {
-		
-		public String solution(String arg){
+
+		public String solution(String S) {
+			String result = "";
+			String[] args = S.split(" ");
+
 			Stack<String> stack = new Stack<>();
-			
-			
-			return null;
+			for (String item : args) {
+				stack.push(item);
+			}
+
+			int spaceCount = stack.size() - 1;
+			while (!stack.isEmpty()) {
+
+				result += stack.pop();
+
+				if (spaceCount > 0) {
+
+					result += " ";
+					spaceCount--;
+
+				}
+
+			}
+
+			System.out.println(result);
+
+			return result;
 		}
-		
+
 	}
 
 }
